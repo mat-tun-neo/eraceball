ca811992b9375845ad48b303cf6f3ba67a635afe
mat-tun-neo <nmatsuo0227@yahoo.co.jp>
2022-03-08 22:27:56 +0900

    commit

--- game_main/js/main.js
+++ game_main/js/main.js
@@ -39,6 +39,7 @@ const ASSETS = {
     "ball_soccer":     "./images/ball_soccer.png" + datestr,
     "ball_animal":     "./images/ball_animal.png" + datestr,
     "ball_alphabet":   "./images/ball_alphabet.png" + datestr,
+    "ball_alphabet2":  "./images/ball_alphabet2.png" + datestr,
     "xbutton":         "./images/xbutton.png" + datestr
   },
   // スプライトシート
@@ -112,6 +113,38 @@ const ASSETS = {
     "ball_alphabet":
     {
       "frame": { "width": 160, "height": 160, "cols": 7, "rows": 4 },
+      "animations" : {
+        "000": {"frames": [0],  "next": "000", "frequency": 1, "name": "エイ"       , "color": "black" },
+        "001": {"frames": [1],  "next": "001", "frequency": 1, "name": "ビー"       , "color": "black" },
+        "002": {"frames": [2],  "next": "002", "frequency": 1, "name": "シー"       , "color": "black" },
+        "003": {"frames": [3],  "next": "003", "frequency": 1, "name": "ディー"     , "color": "black" },
+        "004": {"frames": [4],  "next": "004", "frequency": 1, "name": "イー"       , "color": "black" },
+        "005": {"frames": [5],  "next": "005", "frequency": 1, "name": "エフ"       , "color": "black" },
+        "006": {"frames": [6],  "next": "006", "frequency": 1, "name": "ジー"       , "color": "black" },
+        "007": {"frames": [7],  "next": "007", "frequency": 1, "name": "エイチ"     , "color": "black" },
+        "008": {"frames": [8],  "next": "008", "frequency": 1, "name": "アイ"       , "color": "black" },
+        "009": {"frames": [9],  "next": "009", "frequency": 1, "name": "ジェイ"     , "color": "black" },
+        "010": {"frames": [10], "next": "010", "frequency": 1, "name": "ケイ"       , "color": "black" },
+        "011": {"frames": [11], "next": "011", "frequency": 1, "name": "エル"       , "color": "black" },
+        "012": {"frames": [12], "next": "012", "frequency": 1, "name": "エム"       , "color": "black" },
+        "013": {"frames": [13], "next": "013", "frequency": 1, "name": "エヌ"       , "color": "black" },
+        "014": {"frames": [14], "next": "014", "frequency": 1, "name": "オー"       , "color": "black" },
+        "015": {"frames": [15], "next": "015", "frequency": 1, "name": "ピー"       , "color": "black" },
+        "016": {"frames": [16], "next": "016", "frequency": 1, "name": "キュー"     , "color": "black" },
+        "017": {"frames": [17], "next": "017", "frequency": 1, "name": "アール"     , "color": "black" },
+        "018": {"frames": [18], "next": "018", "frequency": 1, "name": "エス"       , "color": "black" },
+        "019": {"frames": [19], "next": "019", "frequency": 1, "name": "ティー"     , "color": "black" },
+        "020": {"frames": [20], "next": "020", "frequency": 1, "name": "ユー"       , "color": "black" },
+        "021": {"frames": [21], "next": "021", "frequency": 1, "name": "ヴィー"     , "color": "black" },
+        "022": {"frames": [22], "next": "022", "frequency": 1, "name": "ダブリュー" , "color": "black" },
+        "023": {"frames": [23], "next": "023", "frequency": 1, "name": "エックス"   , "color": "black" },
+        "024": {"frames": [24], "next": "024", "frequency": 1, "name": "ワイ"       , "color": "black" },
+        "025": {"frames": [25], "next": "025", "frequency": 1, "name": "ズィー"     , "color": "black" }
+      }
+    },
+    "ball_alphabet2":
+    {
+      "frame": { "width": 290, "height": 300, "cols": 6, "rows": 5 },
       "animations" : {
         "000": {"frames": [0],  "next": "000", "frequency": 1, "name": "エイ"       , "color": "black" },
         "001": {"frames": [1],  "next": "001", "frequency": 1, "name": "ビー"       , "color": "black" },
